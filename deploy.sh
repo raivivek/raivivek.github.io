b155915f2c5aae925f0eaf2f171be160e8571703
Vivek Rai <vivekraiiitkgp@gmail.com>
2016-01-19 15:08:57 +0530

    Update deploy script
    Signed-off-by: Vivek Rai <vivekraiiitkgp@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Ensure that you have rebuilt the site and commited your changes
-echo "Deploying to GitHub pages..."
+echo "Deploying to https://vivekiitkgp.github.io's master..."
 
 # Create the subtree
 git subtree split --prefix site -b gh-pages
 # Push to origin
-git push -f blog gh-pages:gh-pages
+git push -f blog gh-pages:master
 # Remove so that later the process can be repeated
 git branch -D gh-pages
 # Push master
+git push blog shorts
 git push origin master
